b94bfa37792a1e9f97577b58f6e6393073dbefb2
pls11dsp <pls11@dspcontroles.com.br>
2024-05-16 15:26:13 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/pls11dsp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pls11dsp/workshopmongo/resources/UserResource.java
@@ -1,27 +1,34 @@
 package com.pls11dsp.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pls11dsp.workshopmongo.domain.User;
+import com.pls11dsp.workshopmongo.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+		
+		
+		/*@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<User>> findAll(){
+			List<User> list = service.findAll();
+			return ResponseEntity.ok().body(list);*/
+		
 	}
 
 }
