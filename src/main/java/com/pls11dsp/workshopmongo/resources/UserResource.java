a3420cf4fd805b4209b4867651a38b276964df66
pls11dsp <pls11@dspcontroles.com.br>
2024-05-22 05:47:53 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/pls11dsp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pls11dsp/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pls11dsp.workshopmongo.domain.Post;
 import com.pls11dsp.workshopmongo.domain.User;
 import com.pls11dsp.workshopmongo.dto.UserDTO;
 import com.pls11dsp.workshopmongo.services.UserService;
@@ -63,6 +64,12 @@ public class UserResource {
 	obj = service.update(obj);
 	return ResponseEntity.noContent().build();
   }
+	
+	@GetMapping(value="/{id}/posts")   //, method=RequestMethod.GET , //com esta instrução, não funciona.
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+	User obj = service.findById(id);
+	return ResponseEntity.ok().body(obj.getPosts());
+}
 }
 
 
