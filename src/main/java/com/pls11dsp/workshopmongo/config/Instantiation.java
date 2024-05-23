8755ea5affd394eb1a2fb6ce1e8a5ce659c87894
pls11dsp <pls11@dspcontroles.com.br>
2024-05-23 11:47:18 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/pls11dsp/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pls11dsp/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.pls11dsp.workshopmongo.domain.Post;
 import com.pls11dsp.workshopmongo.domain.User;
 import com.pls11dsp.workshopmongo.dto.AuthorDTO;
+import com.pls11dsp.workshopmongo.dto.CommentDTO;
 import com.pls11dsp.workshopmongo.repository.PostRepository;
 import com.pls11dsp.workshopmongo.repository.UserRepository;
 
@@ -44,7 +45,14 @@ public class Instantiation implements CommandLineRunner{
 			  				
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", 
 				     new AuthorDTO(maria));
-				
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
