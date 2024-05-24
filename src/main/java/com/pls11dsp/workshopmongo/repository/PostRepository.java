a00eaf433fdd2a6dae2065b17cbe50ad9814fa1e
pls11dsp <pls11@dspcontroles.com.br>
2024-05-24 08:15:43 -0300

    Consulta simples com @Query

--- src/main/java/com/pls11dsp/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pls11dsp/workshopmongo/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.pls11dsp.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pls11dsp.workshopmongo.domain.Post;
@@ -10,6 +10,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{'Title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
