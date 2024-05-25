da10d0b58fc92a089daa30cc81b74958f1ae4352
pls11dsp <pls11@dspcontroles.com.br>
2024-05-24 21:09:33 -0300

    Consulta com vários critérios

--- src/main/java/com/pls11dsp/workshopmongo/resources/PostResouce.java
+++ src/main/java/com/pls11dsp/workshopmongo/resources/PostResouce.java
@@ -1,5 +1,6 @@
 package com.pls11dsp.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,20 @@ public class PostResouce {
 	List<Post> list = service.findByTitle(text);
 	return ResponseEntity.ok().body(list);
 }
+	
+	@GetMapping(value="/fullsearch")  
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+		    @RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate){
+		
+	text = URL.decoderParam(text);
+	Date min = URL.convertDate(minDate, new Date(0L));
+	Date max = URL.convertDate(maxDate, new Date());
+	List<Post> list = service.fullSearch(text, min, max);
+	return ResponseEntity.ok().body(list);
+}
+	
 }
 
 
